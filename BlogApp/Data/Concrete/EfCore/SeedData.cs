61afb220b6ccc6bc6be2ce45d8c91791a0327c56
Melisa AYDIN <melisa.aydin650@gmail.com>
2025-07-31 21:28:46 +0300

    seed data update

--- BlogApp/Data/Concrete/EfCore/SeedData.cs
+++ BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -1,10 +1,6 @@
 using BlogApp.Entity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlogApp.Data.Concrete.EfCore
 {
@@ -37,7 +33,7 @@ namespace BlogApp.Data.Concrete.EfCore
                 }
             }
 
-            // 3. Find or Create Users (with detailed error logging)
+            // 3. Find or Create Users
             User? adminUser = await userManager.FindByEmailAsync("melisaaydin@gmail.com");
             if (adminUser == null)
             {
@@ -51,7 +47,6 @@ namespace BlogApp.Data.Concrete.EfCore
                 }
                 else
                 {
-                    // HATA DETAYINI YAZDIR
                     logger.LogError("Admin user creation failed: {Errors}", string.Join(", ", result.Errors.Select(e => e.Description)));
                     adminUser = null;
                 }
@@ -69,8 +64,6 @@ namespace BlogApp.Data.Concrete.EfCore
                 }
                 else
                 {
-                    // HATA DETAYINI YAZDIR
-                    logger.LogError("Normal user creation failed: {Errors}", string.Join(", ", result.Errors.Select(e => e.Description)));
                     normalUser = null;
                 }
             }
